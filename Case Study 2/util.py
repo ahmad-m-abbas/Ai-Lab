890755d8e44284dc109b50381e441adcf62b9263
ahmad-m-abbas <ahmad.abbas176@yahoo.com>
2024-02-02 10:30:32 +0200

    Case 2

--- Case Study 2/util.py
+++ Case Study 2/util.py
@@ -18,6 +18,8 @@ import time
 import gc
 import xgboost as xgb
 from sklearn.model_selection import RandomizedSearchCV
+from memory_profiler import profile
+
 class Utils:
     def find_best_rf_parameters(X_train, y_train):
         rf_param_grid = {
@@ -27,7 +29,7 @@ class Utils:
             'max_features': ["sqrt", "log2"]
         }
     
-        random_search = RandomizedSearchCV(RandomForestClassifier(), rf_param_grid, n_iter=10, cv=3, verbose=2, n_jobs=-1)
+        random_search = RandomizedSearchCV(RandomForestClassifier(), rf_param_grid, n_iter=10, cv=3, verbose=0, n_jobs=-1)
         
         start_time = time.time()
         random_search.fit(X_train, y_train)
@@ -36,23 +38,27 @@ class Utils:
         for mean_score, params in zip(random_search.cv_results_['mean_test_score'], random_search.cv_results_['params']):
             print(f"{params} with accuracy: {mean_score}")
     
-        # Find the best model
         best_model = random_search.best_estimator_
         print("\n--- %s seconds ---" % (time.time() - start_time))
         print("The highest Accuracy {:.5f} is the model with parameters: {}".format(random_search.best_score_, random_search.best_params_))
     
         return best_model
 
-    def accuracy_measure_rf(X_train, y_train, X_test, y_test, n_estimators = 50, criterion  = 'entropy', max_depth =10, max_features='log2'):
+    @profile
+    def accuracy_measure_rf(X_train, y_train, X_test, y_test, n_estimators=50, criterion='entropy', max_depth=10, max_features='log2'):
+        import time
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+    
         start_time = time.time()
-        rf_model = RandomForestClassifier(n_estimators = n_estimators, criterion  = criterion, max_depth =max_depth, max_features=max_features)
+        rf_model = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion, max_depth=max_depth, max_features=max_features)
         rf_model.fit(X_train, y_train)
-        rf_accuracy=rf_model.score(X_test, y_test)
         predictions = rf_model.predict(X_test)
-        rf_accuracy= accuracy_score(y_test, predictions)
-        rf_f_score = precision_recall_fscore_support(y_test,predictions)
+        rf_accuracy = accuracy_score(y_test, predictions)
+        rf_f_score = precision_recall_fscore_support(y_test, predictions)
         print("--- %s seconds ---" % (time.time() - start_time))
-        return rf_accuracy, rf_f_score
+        return rf_model, rf_accuracy, rf_f_score
+
 
     def find_best_xgb_parameters(X_train, y_train, X_val, y_val):
         xgb_param_grid = {
@@ -62,28 +68,30 @@ class Utils:
         }
         start_time = time.time()
         xgb_model = xgb.XGBClassifier(enable_categorical=True, early_stopping_rounds=10, n_jobs=-1)
-        random_search = RandomizedSearchCV(xgb_model, xgb_param_grid, n_iter=10, cv=3, verbose=2, n_jobs=2)
+        random_search = RandomizedSearchCV(xgb_model, xgb_param_grid, n_iter=10, cv=3, verbose=0, n_jobs=2)
         random_search.fit(X_train, y_train, eval_set=[(X_val, y_val)], verbose=False)
         print("--- %s seconds ---" % (time.time() - start_time))
-        # Log all results with their accuracies
         print("All Results:")
         for mean_score, params in zip(random_search.cv_results_['mean_test_score'], random_search.cv_results_['params']):
             print(f"{params} with accuracy: {mean_score}")
     
-        # Best Model Information
         best_model = random_search.best_estimator_
         print("\nBest parameters found: ", random_search.best_params_)
         print("Best accuracy found: ", random_search.best_score_)
     
         return best_model
-    def accuracy_measure_xgb(X_train, y_train, X_test, y_test, n_estimators = 50, learning_rate  = 0.1, max_depth =10, subsample=0.5, colsample_bytree=0.5):
-        start_time = time.time()
-        xgb_model = xgb.XGBClassifier(n_estimators = n_estimators, learning_rate  = learning_rate, max_depth =max_depth, subsample=subsample, colsample_bytree=colsample_bytree)
-        xgb_model.fit(X_train, y_train)
-        xgb_accuracy=xgb_model.score(X_test, y_test)
-        predictions = xgb_model.predict(X_test)
-        xgb_accuracy= accuracy_score(y_test, predictions)
-        xgb_f_score = precision_recall_fscore_support(y_test,predictions)
-        print("--- %s seconds ---" % (time.time() - start_time))
-        return xgb_accuracy, xgb_f_score
+    @profile
+    def accuracy_measure_xgb(X_train, y_train, X_test, y_test, n_estimators=50, learning_rate=0.1, max_depth=10, subsample=0.5, colsample_bytree=0.5):
+        import time
+        import xgboost as xgb
+        from sklearn.metrics import accuracy_score, precision_recall_fscore_support
     
+        start_time = time.time()
+        xgb_model = xgb.XGBClassifier(n_estimators=n_estimators, learning_rate=learning_rate, max_depth=max_depth, subsample=subsample, colsample_bytree=colsample_bytree)
+        xgb_model.fit(X_train, y_train)
+        predictions = xgb_model.predict(X_test)
+        xgb_accuracy = accuracy_score(y_test, predictions)
+        xgb_f_score = precision_recall_fscore_support(y_test, predictions)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        return xgb_model, xgb_accuracy, xgb_f_score
+        
